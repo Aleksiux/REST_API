affdfd699f1e1f1cacb70b3b3a21701d06356930
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-05-23 21:54:13 +0300

    adding add/edit/update/delete/count comments

--- django_api/views.py
+++ django_api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post, Comment, PostLike, CommentLike
-from .serializers import PostSerializer
+from rest_framework.exceptions import ValidationError
+from .serializers import PostSerializer, CommentSerializer
 
 
 # class PostList(generics.ListAPIView):
@@ -16,3 +17,57 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def put(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=request.user)
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError('You can\'t edit another user post!')
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('You can\'t delete another user post!')
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Comment.objects.filter(post=post)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def put(self, request, *args, **kwargs):
+        comment = Comment.objects.filter(pk=kwargs['pk'], user=request.user)
+        if comment.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError('You can\'t edit another user comments!')
+
+    def delete(self, request, *args, **kwargs):
+        comment = Comment.objects.filter(pk=kwargs['pk'], user=request.user)
+        if comment.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('You can\'t delete another user comments!')
